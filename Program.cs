b179fdd33daacc9e02823a58004b7d3f8c0066ae
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-06 10:04:32 -0700

    Cab Invoice Generator

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace CabInvoiceGenerator
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To cab Invoice Generator");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double flare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine($"Fare : {fare}");
+        }
+    }
+}
